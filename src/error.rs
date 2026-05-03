438ee2f36d9f6f1b06550b512ebd80ce4a98291b
Luis Schwab <luisschwab@protonmail.com>
2026-05-03 14:58:51 -0300

    chore(error): drop `thiserror`

--- src/error.rs
+++ src/error.rs
@@ -1,33 +1,75 @@
+use core::error;
+use core::fmt;
+use std::io;
+
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::response::Response;
-use thiserror::Error;
 
-#[derive(Error, Debug)]
+#[derive(Debug)]
 pub(crate) enum KoerierError {
     /// Error reading file from the file system.
-    #[error("Error reading {0} from the file system")]
-    FsError(#[from] std::io::Error),
-
+    FsError(io::Error),
     /// Error parsing PEM certificate from LND.
-    #[error("Error parsing PEM certificate")]
-    CertError(#[from] reqwest::Error),
-
+    CertError(reqwest::Error),
     /// Error fetching payment request from LND.
-    #[error("Error fetching payment request from LND: {0}")]
     Lnd(String),
-
     /// Error opening image from the file system.
-    #[error("Error opening image: {0}")]
-    Image(#[from] image::error::ImageError),
-
+    Image(image::error::ImageError),
     /// Error serializing into JSON.
-    #[error("Error serializing into JSON: {0}")]
-    Json(#[from] serde_json::Error),
+    Json(serde_json::Error),
+}
+
+impl fmt::Display for KoerierError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::FsError(e) => write!(f, "Error reading {e} from the file system"),
+            Self::CertError(_) => write!(f, "Error parsing PEM certificate"),
+            Self::Lnd(msg) => write!(f, "Error fetching payment request from LND: {msg}"),
+            Self::Image(e) => write!(f, "Error opening image: {e}"),
+            Self::Json(e) => write!(f, "Error serializing into JSON: {e}"),
+        }
+    }
+}
+
+impl error::Error for KoerierError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            Self::FsError(e) => Some(e),
+            Self::CertError(e) => Some(e),
+            Self::Lnd(_) => None,
+            Self::Image(e) => Some(e),
+            Self::Json(e) => Some(e),
+        }
+    }
+}
+
+impl From<io::Error> for KoerierError {
+    fn from(e: io::Error) -> Self {
+        Self::FsError(e)
+    }
+}
+
+impl From<reqwest::Error> for KoerierError {
+    fn from(e: reqwest::Error) -> Self {
+        Self::CertError(e)
+    }
+}
+
+impl From<image::error::ImageError> for KoerierError {
+    fn from(e: image::error::ImageError) -> Self {
+        Self::Image(e)
+    }
+}
+
+impl From<serde_json::Error> for KoerierError {
+    fn from(e: serde_json::Error) -> Self {
+        Self::Json(e)
+    }
 }
 
 impl IntoResponse for KoerierError {
     fn into_response(self) -> Response {
-        (StatusCode::INTERNAL_SERVER_ERROR, format!("{}", self)).into_response()
+        (StatusCode::INTERNAL_SERVER_ERROR, self.to_string()).into_response()
     }
 }
